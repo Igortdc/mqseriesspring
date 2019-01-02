8d6e596b08f19c13729b457f4bd00bf7e9841a9b
Igor Teixeira da Costa <igortc@cit.local>
2019-01-02 13:25:08 -0200

    Funcionando envio e recebimento mensagem

--- src/main/java/com/example/mqspring/MqspringApplication.java
+++ src/main/java/com/example/mqspring/MqspringApplication.java
@@ -1,14 +1,74 @@
 package com.example.mqspring;
 
+import javax.jms.ConnectionFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableJms
 @SpringBootApplication
 public class MqspringApplication {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		// This provides all boot's default to this factory, including the
+		// message converter
+		configurer.configure(factory, connectionFactory);
+		// You could still override some of Boot's default if necessary.
+		return factory;
+	}
+
+	@Bean // Serialize message content to json using TextMessage
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MqspringApplication.class, args);
 	}
 
-}
+	@GetMapping("send")
+	String send(@RequestParam("msg") String msg) {
+		try {
+			jmsTemplate.convertAndSend("DEV.QUEUE.1", msg);
+			return "OK";
+		} catch (JmsException ex) {
+			ex.printStackTrace();
+			return "FAIL";
+		}
+	}
 
+	@GetMapping("recv")
+	String recv() {
+		try {
+			return jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
+		} catch (JmsException ex) {
+			ex.printStackTrace();
+			return "FAIL";
+		}
+	}
+
+}
