3390b1588603831ab508ada13bb069026613b274
Igor Teixeira da Costa <igortc@cit.local>
2019-01-02 18:32:39 -0200

    Configurações da jms

--- src/main/java/com/example/mqspring/MqspringApplication.java
+++ src/main/java/com/example/mqspring/MqspringApplication.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 @EnableJms
 @SpringBootApplication
